8d8026829c9b94cc99f42016c78e186157a69f58
sangho44-lee <sangho44@gmail.com>
2025-07-08 11:04:55 +0900

    프로그램 수정 후 재업로드

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -7,7 +7,7 @@ from airflow.providers.standard.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_with_macro_eg1",
     schedule="10 0 L * *",
-    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     # START DATE : 전월말일, END_DATE : 1일전   
