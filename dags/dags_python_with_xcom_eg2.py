d3882d934bba8d67449098add0d8e1c1684fb75b
sangho44-lee <sangho44@gmail.com>
2025-07-08 14:44:38 +0900

    err 수정후 업로드

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -28,6 +28,6 @@ with DAG(
         
     python_xcom_push_by_return = xcom_push_result()
     xcom_pull_2(python_xcom_push_by_return)
-    python_xcom_push_by_return >> python_xcom_pull_1()
+    python_xcom_push_by_return >> xcom_pull_1()
         
 
