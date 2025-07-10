39e31d8b86fe2929c99a8c9b2b328373c287f92f
sangho44-lee <sangho44@gmail.com>
2025-07-10 16:10:41 +0900

    modify pgm load

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -3,6 +3,8 @@ import datetime
 import pendulum
 from airflow.operators.python import PythonOperator
 
+
+
 with DAG(
     dag_id="dags_python_with_postgres",
     start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
@@ -14,17 +16,23 @@ with DAG(
         import psycopg2
         from contextlib import closing
         
-        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
+        with closing(psycopg2.connect(
+            host=ip, 
+            dbname=dbname, 
+            user=user, 
+            password=passwd, 
+            port=int(port))
+        ) as conn:
             with closing(conn.cursor()) as cursor:
                 dag_id = kwargs.get('ti').dag_id
-                task_id = kwargs.get('ti').task_id
+                task_id = kwargs.get('ti').task_id 
                 run_id = kwargs.get('ti').run_id
                 msg = 'insrt 수행'
                 sql = 'insert into py_opr_drct_insrt values(%s, %s, %s, %s);'
                 cursor.execute(sql, (dag_id, task_id, run_id, msg))
                 conn.commit()
                 
-    insrt_postgres= pythonOperator(
+    insrt_postgres= PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
         op_args=['172.28.0.3', '5433', 'shlee', 'shlee','shlee']
