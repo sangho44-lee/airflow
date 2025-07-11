98b2766d4b10c8fae9e9aa4cf2dd6d7543ed9dc3
sangho44-lee <sangho44@gmail.com>
2025-07-11 13:38:16 +0900

    modify source pgm reloadR

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -1,16 +1,14 @@
 from airflow import DAG
 import datetime
 import pendulum
-from airflow.operators.python import PythonOperator
-
-
+from airflow.operators.python import PythonOperator # type: ignore   
 
 with DAG(
     dag_id="dags_python_with_postgres",
     start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
     schedule=None,
     catchup=False
-) as dag:
+) as dag: 
     
     def insrt_postgres(ip, port, dbname,user, passwd, **kwargs):
         import psycopg2
